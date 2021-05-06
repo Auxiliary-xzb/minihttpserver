b4d887c39fcb69710107fa7543a4f89075ec5602
= <=>
2021-05-06 16:54:04 +0800

    no comment in code

--- test.c
+++ test.c
@@ -1,17 +1,13 @@
 #include "utilities.h"
 
 int main(int arg, char *argv[]) {
-    int lisfd = getListener(9527);
-printf("hahah");   
-    eventTree = getEventTree(300); 
-printf("hahah");   
+    int lisfd = getListener(9527);  
+    eventTree = getEventTree(300);    
     thread_pool = getThreadPool(10, 30, 50);
-    for (int i = 0; i < 5; i++) {
+
 	event_t *new = getNewEvent(lisfd, EPOLLIN, acceptConnection);
-	
 	addEvent(eventTree, new);
-    }
-    
+  
     startEventListener(eventTree);
     return 0;
 }
